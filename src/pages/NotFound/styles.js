7e7a6474690e46f32ecdff9ff00cd0db31c519cd
epcshin <francisco.shin@hotmail.com>
2023-04-10 15:35:25 -0300

    testando

---
+++ src/pages/NotFound/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const H1p = styled.h1`
+  width: 100%;
+  height: 30%;
+  font-size: 36px;
+  color: #ccc;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const stylesCss = {
+  width: '100%',
+  heigh: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+export { H1p, stylesCss };
